1f7bc972611aa99dcd1ca098f733e2e576d720c0
gauravkardam3 <gakardam@LIN51014697.corp.capgemini.com>
2021-09-20 11:27:17 +0530

    Code

--- samplerepo/src/main/java/samplerepo/ConflictCode.java
+++ samplerepo/src/main/java/samplerepo/ConflictCode.java
@@ -1,5 +1,17 @@
 package samplerepo;
 
 public class ConflictCode {
-
+	 public static void main(String[] args) {  
+		 System.out.println("This is the main Program");
+	        
+	    }
+	 
+	 public static void printArray() {
+		 int [] arr = new int [] {1, 2, 3, 4, 5};  
+	        System.out.println("Elements of given array: ");  
+	        for (int i = 0; i < arr.length; i++) {  
+	            System.out.print(arr[i] + " ");  
+	        }  
+		 
+	 }
 }
