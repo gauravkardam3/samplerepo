93024a238ddb5061535dfea0854386bc61c5e5ae
gauravkardam3 <gakardam@LIN51014697.corp.capgemini.com>
2021-09-20 11:33:11 +0530

    updated

--- samplerepo/src/main/java/samplerepo/ConflictCode.java
+++ samplerepo/src/main/java/samplerepo/ConflictCode.java
@@ -7,7 +7,9 @@ public class ConflictCode {
 	    }
 	 
 	 public static void printArray() {
-		 int [] arr = new int [] {1, 2, 3, 4, 5};  
+		 int [] arr = new int [] {11,22,33,55,77};  
+		 int sum=0;
+		 sum = 15;
 	        System.out.println("Elements of given array: ");  
 	        for (int i = 0; i < arr.length; i++) {  
 	            System.out.print(arr[i] + " ");  
