7b11b18a1b714186bc1355419b899b82bd92ff80
gauravkardam3 <gakardam@LIN51014697.corp.capgemini.com>
2021-09-20 11:29:26 +0530

    Merge branch 'master' of https://github.com/gauravkardam3/samplerepo.git

--- samplerepo/src/main/java/samplerepo/ConflictCode.java
+++ samplerepo/src/main/java/samplerepo/ConflictCode.java
@@ -1,5 +1,18 @@
 package samplerepo;
 
 public class ConflictCode {
-
+	 public static void main(String[] args) {  
+		 System.out.println("This is the main Program");
+	        
+	    }
+	 
+	 public static void printArray() {
+		 int [] arr = new int [] {5,6,7,8,9,1,2,3};  
+		 int sum=0;
+	        System.out.println("Elements of given array: ");  
+	        for (int i = 0; i < arr.length; i++) {  
+	            System.out.print(arr[i] + " ");  
+	        }  
+		 
+	 }
 }
